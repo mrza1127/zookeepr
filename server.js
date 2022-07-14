f4fd698811767569a8adbba2035d6b453bfe5f6e
isaias <104476799+mrza1127@users.noreply.github.com>
2022-07-14 17:14:44 -0500

    updated front end

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming strings or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -113,6 +114,24 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// the '*' is a wildcard route, no matter what is typed in, it will route to whatever was coded
+// this '*' route should always come last
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
